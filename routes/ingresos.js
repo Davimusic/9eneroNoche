1d2da99cbfe85f1de836a9326324d56e736a1527
DaviMusic <davipianof@gmail.com>
2022-01-09 21:55:27 -0500

    3

--- routes/ingresos.js
+++ routes/ingresos.js
@@ -4,40 +4,40 @@ const router = express.Router()
 const alumnoController = require('../controllers/ingresoController')
 
 //mostrar todos los alumnos (GET)
-router.get('/', alumnoController.mostrar)
+router.get('https://enero9noche.herokuapp.com/', alumnoController.mostrar)
 //crear alumno (POST)
-router.post('/crear', alumnoController.crear)
+router.post('https://enero9noche.herokuapp.com/crear', alumnoController.crear)
 //edita alumno(POST)
-router.post('/editar', alumnoController.editar)
+router.post('https://enero9noche.herokuapp.com/editar', alumnoController.editar)
 //elimina (GET)
-router.get('/borrar/:id', alumnoController.borrar)
+router.get('https://enero9noche.herokuapp.com/borrar/:id', alumnoController.borrar)
 
 //mio, muestra información filtrada en el index
-router.post('/otroMostrar', alumnoController.mostrarTodo)
+router.post('https://enero9noche.herokuapp.com/otroMostrar', alumnoController.mostrarTodo)
 
 //ingreso arriendos
 //promedio anual
-router.get('/promedios', alumnoController.promedios)
+router.get('https://enero9noche.herokuapp.com/promedios', alumnoController.promedios)
 //promedios mensuales
-router.get('/vistaMensual', alumnoController.vistaMensual)
+router.get('https://enero9noche.herokuapp.com/vistaMensual', alumnoController.vistaMensual)
 
 //para ingresos externos
-router.post('/crearGananciasExternas', alumnoController.crearGananciasExternas)
-router.get('/mostrarGananciasExternas', alumnoController.mostrarGananciasExternas)
-router.post('/editarIngresoExterno', alumnoController.editarIngresoExterno)
-router.post('/filtroIngresosExternos', alumnoController.filtroIngresosExternos)
-router.get('/mostrarVistaMenAnulIngresosExternos', alumnoController.mostrarMesAñoIngresoExterno)
+router.post('https://enero9noche.herokuapp.com/crearGananciasExternas', alumnoController.crearGananciasExternas)
+router.get('https://enero9noche.herokuapp.com/mostrarGananciasExternas', alumnoController.mostrarGananciasExternas)
+router.post('https://enero9noche.herokuapp.com/editarIngresoExterno', alumnoController.editarIngresoExterno)
+router.post('https://enero9noche.herokuapp.com/filtroIngresosExternos', alumnoController.filtroIngresosExternos)
+router.get('https://enero9noche.herokuapp.com/mostrarVistaMenAnulIngresosExternos', alumnoController.mostrarMesAñoIngresoExterno)
 
 //para dineros salientes
-router.get('/mostrarDinerosSalida', alumnoController.mostrarDinerosSalida)
-router.post('/crearSalidaDinero', alumnoController.crearSalidaDinero)
-router.post('/editarSalidaDinero', alumnoController.editarSalidaDinero)
-router.post('/filtroSalidaDinero', alumnoController.filtroSalidaDinero)
-router.get('/mostrarMesAnoSalidaDinero', alumnoController.mostrarMesAnoSalidaDinero)
+router.get('https://enero9noche.herokuapp.com/mostrarDinerosSalida', alumnoController.mostrarDinerosSalida)
+router.post('https://enero9noche.herokuapp.com/crearSalidaDinero', alumnoController.crearSalidaDinero)
+router.post('https://enero9noche.herokuapp.com/editarSalidaDinero', alumnoController.editarSalidaDinero)
+router.post('https://enero9noche.herokuapp.com/filtroSalidaDinero', alumnoController.filtroSalidaDinero)
+router.get('https://enero9noche.herokuapp.com/mostrarMesAnoSalidaDinero', alumnoController.mostrarMesAnoSalidaDinero)
 
 //para validar logeo
-router.get('/abrirPaginaDeLogeo', alumnoController.abrirPaginaDeLogeo)
-router.post('/validarLogeo', alumnoController.validarLogeo)
+router.get('https://enero9noche.herokuapp.com/abrirPaginaDeLogeo', alumnoController.abrirPaginaDeLogeo)
+router.post('https://enero9noche.herokuapp.com/validarLogeo', alumnoController.validarLogeo)
 
 
 
