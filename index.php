dbaa20b113bcf09ccf6dc79a808bd32f74b95be6
DaviMusic <davipianof@gmail.com>
2022-01-09 21:52:24 -0500

    version32

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-holaaaaa x2 a php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <div class="menuCelular">
+            <form method="link" action="/promedios">
+                <input class="menuCel" type="submit" value="IR  A PROMEDIOS">
+            </form>
+        </div>
+</body>
+</html>
